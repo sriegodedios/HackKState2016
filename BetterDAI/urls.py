380e1f3e2f806aaf49df6598c22399b69a5c5e2c
mslavens <mslavens@ksu.edu>
2016-11-12 01:59:34 -0600

    fixed bugs in url paths

--- BetterDAI/urls.py
+++ BetterDAI/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.index, name='index'), #Default page
 ]
--- BetterDAI/views.py
+++ BetterDAI/views.py
@@ -5,10 +5,12 @@ from django.contrib import auth
 # from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 
-
+import os
+import sys
 # Create your views here.
-
+from django.conf.urls import include, url
+from django.contrib import admin
 
 
 def index(request):
-    return render('index.html');
+    return render(request, 'index.html');
