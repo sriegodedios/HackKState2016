861ddd5c742c079b55e66c111b2e00395f596e93
YouliangYu <youliang1229@gmail.com>
2016-11-13 12:01:49 -0600

    final commit for ML models

--- ensemble.R
+++ ensemble.R
@@ -4,83 +4,46 @@
 #date: "Nov. 11, 2016"                                                     ##      
 #############################################################################
 
-rm(list=ls())
-#load data
+rm(list=ls()) 
+#build models and ensemble")
 library(caret)
 library(data.table)  
-train <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
-#test <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
-
-#get tsne features
-train <- train[!is.na(train$readmitted),] # remove NA label samples
-y = as.integer(as.factor(train$readmitted))-1;trainID <- train[, 2]
-train <- train[,-c(1,2)] # remove IDs
-#apply(train, 2, function(x) sum(is.na(x))/nrow(train)*100)
-cst_vars <- names(train)[apply(train, 2, function(x) length(unique(x))==1)]; for (f in cst_vars){train[[f]] <- NULL} 
-train$readmitted <- y; medi_vars <- names(train[,c(23:40)]) 
-
-for(f in names(train)) { 
-  if(is.character(train[[f]])) {
-    #    levels <- unique(test[[f]])
-    #    target.means <- unlist(lapply(levels, function(x) x=mean(train[,"readmitted"][train[,f]==x],na.rm = T)))
-    #    names(target.means) <- levels
-    #    tmp <- rep(NA, nrow(test))
-    #    tmp[!is.na(test[[f]])] <- unlist(lapply(test[[f]][!is.na(test[[f]])], function(x) x = target.means[[x]]))
-    #    test[[f]] <- tmp
-    temp <- rep(NA, nrow(train)) 
-    for(i in 1:5) {
-      ids.1 <- -seq(i, nrow(train), by=5)
-      ids.2 <- seq(i, nrow(train), by=5)
-      levels <- unique(train[ids.2,f])
-      target.means <- unlist(lapply(levels, function(x) x=mean(train[ids.1,"readmitted"][train[ids.1,f]==x],na.rm = T)))
-      names(target.means) <- levels
-      temp[ids.2][!is.na(train[ids.2,f])] <- unlist(lapply(train[ids.2,f][!is.na(train[ids.2,f])], function(x) x = target.means[[x]]))
-    }
-    train[[f]] <- temp
-  }
-}
-
-train[is.na(train)] <- 0
-train$readmitted <- NULL
-  
-library(Rtsne)
-tsne_targetmean<- Rtsne(as.matrix(rbind(train)), check_duplicates =FALSE, PCA =T, verbose=TRUE,
-                  perplexity=30, theta=0.5, dims=2, max_iter=800)
-#palette(c("red", "blue"))
-#target = factor(y)
-#qplot(tsne_targetmean$Y[,1],tsne_targetmean$Y[,2], xlab="Y1", ylab="Y2") + aes(shape = target)+aes(colour = target)+ scale_shape(solid = FALSE) 
-
- 
-cat("Build model and ensemble")
 library(xgboost)
 library(ranger)
 library(Metrics)
-
+library(doMC)
+library(glmnet)
+ 
 train <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
-#test <- fread('data/Challenge_1_Testing.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
+test <- fread('data/Challenge_1_Validation.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
 train <- train[!is.na(train$readmitted),]
 trainID <- train$patient_nbr;y = as.integer(as.factor(train$readmitted))-1
 train$encounter_id <- train$patient_nbr <- train$readmitted  <- NULL
-#test$encounter_id <- test$patient_nbr <- NULL
+testID <- test$patient_nbr;test$encounter_id <- test$patient_nbr <- NULL
 
 #remove constant vars
-cst_vars <- names(train)[apply(train, 2, function(x) length(unique(x))==1)]
-for (f in cst_vars){train[[f]] <- NULL} #;test[[f]] <- NULL}
-medi_vars <- names(train[,c(23:40)]) 
+cst_vars <- union(names(train)[apply(train, 2, function(x) length(unique(x))==1)],
+                  names(test)[apply(test, 2, function(x) length(unique(x))==1)])
+
+for (f in cst_vars){train[[f]] <- NULL;test[[f]] <- NULL}
+#medi_vars <- names(train[,c(23:40)]) 
 
 #factorize cate variables
-fact_vars <- names(train)[sapply(train, is.character)]
+fact_vars <- names(test)[sapply(test, is.character)] #;names(train)[sapply(train, is.character)]
 for (f in fact_vars) {
   if (class(train[[f]])=="character") {
-    levels <- unique(c(train[[f]]))#, test[[f]]))
+    levels <- unique(c(train[[f]], test[[f]]))
     train[[f]] <- as.integer(factor(train[[f]], levels=levels))
-#    test[[f]]  <- as.integer(factor(test[[f]],  levels=levels))
+    test[[f]]  <- as.integer(factor(test[[f]],  levels=levels))
   }
 }
 
-#impute NA
+# treat NA
+# get features with all NA(>99.99%)
+# na_var <- names(train)[apply(train, 2, function(x)  sum(is.na(x))/nrow(train)>0.9999)]
 train$NAnbr <- apply(train,1, function(x) sum(is.na(x)))
-train[is.na(train)] <- 0#;test[is.na(test)] <- 0
+test$NAnbr <- apply(test,1, function(x) sum(is.na(x)))
+train[is.na(train)] <- 0;test[is.na(test)] <- 0
 
 #create stratified 5 folds for CV
 set.seed(1229)
@@ -91,10 +54,10 @@ for (k in seq(fold5)) {xfolds[fold5[[k]],2] <- k}# column 2 gets the {1,2,3,4,5}
 colnames(xfolds) <- c("ID", "fold5"); xfolds <- as.data.frame(xfolds)
 nfolds <- length(unique(xfolds$fold5))
 
-#### building models ####
-modelNum <- 4;i=0
+# start building model
+modelNum <- 20;i=0
 mtrain <- array(0, c(nrow(train),modelNum))
-#mtest <- array(0, c(nrow(test),length(modelNum)))
+mtest <- array(0, c(nrow(test),modelNum))
 
 #XGB 1
 i=i+1;
@@ -102,7 +65,7 @@ param <- list(booster ="gbtree", objective = 'binary:logistic', eval_metric = 'r
               nthread = 6,eta = 0.02, colsample_bytree = 0.5, subsample = 0.8, max_depth = 6, min_child_weight=11)
 dtrain <- xgb.DMatrix(as.matrix(cbind(train)), label = y)
 bst.cv = xgb.cv(param=param, data = dtrain, nfold = 5, nrounds = 5000, early.stop.round = 80, set.seed(1229))
-tmp <- bst.cv$test.rmse.mean; n=which(tmp==min(tmp))[1]
+tmp <- bst.cv$test.rmse.mean; n=which(tmp==min(tmp))[1]#418
 # loop over folds
 for (j in 1:nfolds){ 
   indexTrain <- which(xfolds$fold5 != j)
@@ -114,42 +77,15 @@ for (j in 1:nfolds){
   mtrain[indexValid,i] <- predict(clf, as.matrix(train[indexValid,]))
   cat(rmse(y[indexValid],mtrain[indexValid,i]))
 }
-rmse(y,mtrain[,i])
+cat(rmse(y,mtrain[,i]))
 #full version
 set.seed(1229)
 x0d <- xgb.DMatrix(as.matrix(train), label = y)
 clf <- xgb.train(params=param, data=x0d, nround = as.integer(n/0.8))
-#mtest[,i] <- predict(clf, as.matrix(test))
-
-#one-hot-encode
-encode_vars <- names(train)[apply(rbind(train), 2, function(x) length(unique(x))<100)]
-for (f in union(encode_vars,fact_vars)){ 
-  dummyfact <- as.factor(c(-100000,train[[f]]))#,testx[[f]]))
-  temp <- model.matrix(~dummyfact)
-  k=0
-  for (j in 2:ncol(temp)){
-    colnames(temp)[j] <- paste0(f,"Dummy",k)
-    k <- k+1
-  }
-  train <- cbind(train,temp[2:(nrow(train)+1),-1])
-#  test <- cbind(test,temp[(nrow(test)+2):nrow(temp),-1])
-  rm(temp);rm(dummyfact);gc()
-}
-#for (f in union(encode_vars,fact_vars)){train[[f]]<-NULL}#;test[[f]]<-NULL}
-#0.01, train-rmse:0.402431+0.000736	test-rmse:0.419231+0.003265 ***best*** 
+mtest[,i] <- predict(clf, as.matrix(test))
 
 #ranger 1
 i = i+1;
-ranger.model <- ranger(dependent.variable.name = "y", 
-                         data = cbind(y,train),
-                         mtry = 30, 
-                         num.trees = 250,
-                         write.forest = T,
-                         min.node.size = 1,
-                         seed = 1229) 
-#  pred_full <- predict(ranger.model, test)$predictions
-#  mtest[,i] <- pred_full
-#  rm(ranger.model);rm(pred_full);gc();
 #  loop over folds
 for (j in 1:nfolds){
   indexTrain <- which(xfolds$fold5 != j)
@@ -165,7 +101,54 @@ for (j in 1:nfolds){
   mtrain[indexValid,i] <- pred_valid
   cat(rmse(y[indexValid],pred_valid)) 
 }
-rmse(y,mtrain[,i]) # [1] 0.42110
+cat(rmse(y,mtrain[,i])) # [1] 0.42110
+# full version
+ranger.model <- ranger(dependent.variable.name = "y", 
+                       data = cbind(y,train),
+                       mtry = 30, 
+                       num.trees = 300,
+                       write.forest = T,
+                       min.node.size = 1,
+                       seed = 1229) 
+mtest[,i] <- predict(ranger.model, test)$predictions
+#rm(ranger.model);rm(pred_full);gc();
+
+# glmnet 1
+# for (f in na_var){train[[f]] <- NULL};test[[f]] <- NULL} #rm variables most with NAs
+lambda <-10^seq(-4,-1,0.02) 
+registerDoMC(cores=7)
+i=i+1
+for (j in 1:nfolds){  
+  indexTrain <- which(xfolds$fold5 != j)
+  indexValid <- which(xfolds$fold5 == j)  
+  clf <- cv.glmnet(y=y[indexTrain], x=as.matrix(train[indexTrain,]),
+                   family="binomial",type.measure="mse", nfolds =4,parallel=TRUE,lambda=lambda)
+  qplot(clf$lambda,clf$cvm)
+  pred_valid <- predict(clf,newx=as.matrix(train[indexValid,]), s="lambda.min",type="response")
+  mtrain[indexValid,i] <- pred_valid
+  cat(rmse(y[indexValid],mtrain[indexValid,i])) #0.4848158
+}
+cat(rmse(y,mtrain[,i]))
+#full version
+clf <- cv.glmnet(y=y,x=as.matrix(train),
+                 family="binomial",type.measure="auc", nfolds = 5,parallel=TRUE,lambda=lambda)
+qplot(clf$lambda,clf$cvm)
+mtest[,i] <- predict(clf,newx=as.matrix(test), s="lambda.min",type="response")
+
+#one-hot-encode
+encode_vars <- names(train)[apply(rbind(train), 2, function(x) length(unique(x))<100)]
+for (f in intersect(names(train),union(encode_vars,fact_vars))){
+  dummyfact <- as.factor(c(-100000,train[[f]],test[[f]]))
+  temp <- model.matrix(~dummyfact)
+  k=0
+  for (j in 2:ncol(temp)){
+    colnames(temp)[j] <- paste0(f,"Dummy",k)
+    k <- k+1
+  }
+  train <- cbind(train,temp[2:(nrow(train)+1),-1])
+  test <- cbind(test,temp[(nrow(train)+2):nrow(temp),-1])
+  rm(temp);rm(dummyfact);gc()
+} 
 
 #XGB 2
 i=i+1;
@@ -185,66 +168,95 @@ for (j in 1:nfolds){
   mtrain[indexValid,i] <- predict(clf, as.matrix(train[indexValid,]))
   cat(rmse(y[indexValid],mtrain[indexValid,i]))
 }
-rmse(y,mtrain[,i])
+cat(rmse(y,mtrain[,i]))
 #full version
 set.seed(1229)
 x0d <- xgb.DMatrix(as.matrix(train), label = y)
 clf <- xgb.train(params=param, data=x0d, nround = as.integer(n/0.8))
-#mtest[,i] <- predict(clf, as.matrix(test))
+mtest[,i] <- predict(clf, as.matrix(test))
 
-# glmnet
+#ranger 2
+i = i+1;
+ranger.model <- ranger(dependent.variable.name = "y", 
+                         data = cbind(y,train),
+                         mtry = 30, 
+                         num.trees = 300,
+                         write.forest = T,
+                         min.node.size = 1,
+                         seed = 1229)
+pred_full <- predict(ranger.model, test)$predictions
+mtest[,i] <- pred_full
+#  loop over folds
+for (j in 1:nfolds){
+  indexTrain <- which(xfolds$fold5 != j)
+  indexValid <- which(xfolds$fold5 == j)
+  ranger.model <- ranger(dependent.variable.name = "y", 
+                         data = cbind(y,train)[indexTrain,],
+                         mtry = 30, #probability = T,
+                         num.trees = 250,
+                         write.forest = T,
+                         min.node.size = 1,
+                         set.seed(1229))
+  pred_valid <- predict(ranger.model, train[indexValid,])$predictions
+  mtrain[indexValid,i] <- pred_valid
+  cat(rmse(y[indexValid],pred_valid)) 
+}
+cat(rmse(y,mtrain[,i]))
+
+# glmnet 2
 # clean data again for numeric variables...
 train <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
-#test <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
+test <- fread('data/Challenge_1_Validation.csv',header =TRUE,stringsAsFactors = FALSE,na.strings=c("?","No","NO","None",""),data.table= FALSE)
 train <- train[!is.na(train$readmitted),] # remove NA label samples
-y = as.integer(as.factor(train$readmitted))-1;trainID <- train[, 2]
-train <- train[,-c(1,2)] # remove IDs
+trainID <- train$patient_nbr;y = as.integer(as.factor(train$readmitted))-1
+train$encounter_id <- train$patient_nbr <- train$readmitted  <- NULL
+testID <- test$patient_nbr;test$encounter_id <- test$patient_nbr <- NULL
+ 
 #apply(train, 2, function(x) sum(is.na(x))/nrow(train)*100)
-cst_vars <- names(train)[apply(train, 2, function(x) length(unique(x))==1)]; for (f in cst_vars){train[[f]] <- NULL} 
-train$readmitted <- y; #medi_vars <- names(train[,c(23:40)]) 
+cst_vars <- union(names(train)[apply(train, 2, function(x) length(unique(x))==1)],
+                  names(test)[apply(test, 2, function(x) length(unique(x))==1)])
+for (f in cst_vars){train[[f]] <- NULL; test[[f]] <- NULL}
 train <- train[, !names(train) %in% fact_vars];train$readmitted <- NULL
-#na_vars <- names(train)[apply(train, 2, function(x) sum(is.na(x))/nrow(train)*100 == 0)]
-
-library(doMC)
-library(glmnet)
-lambda <-10^seq(-8,-1,0.1) 
-registerDoMC(cores=7)
+test <- test[, !names(test) %in% fact_vars];test$readmitted <- NULL
+ 
 i=i+1
+lambda <-10^seq(-3,-1.5,0.005) 
+registerDoMC(cores=7)
 for (j in 1:nfolds){  
     indexTrain <- which(xfolds$fold5 != j)
     indexValid <- which(xfolds$fold5 == j)  
-    clf <- cv.glmnet(y=y[indexTrain], x=as.matrix(mtrain[indexTrain,]),
-                     family="binomial",type.measure="mse", nfolds =4,parallel=TRUE,lambda=lambda)
+    clf <- cv.glmnet(y=y[indexTrain], x=as.matrix(train[indexTrain,]),
+                     family="binomial",type.measure="auc", nfolds =4,parallel=TRUE,lambda=lambda)
     qplot(clf$lambda,clf$cvm)
-    pred <- predict(clf,newx=as.matrix(mtrain[indexValid,]), s="lambda.min",type="response")
+    pred <- predict(clf,newx=as.matrix(train[indexValid,]), s="lambda.min",type="response")
     mtrain[indexValid,i] <- pred
-    cat(rmse(y[indexValid],mtrain[indexValid,i])) #0.4848158
-#    rm(indexTrain);rm(indexValid);rm(clf);rm(pred);gc()
+    cat(rmse(y[indexValid],mtrain[indexValid,i])) 
 }
-rmse(y,mtrain[,i])
-#full version
-pred <- matrix(0, nrow(test))
-clf <- cv.glmnet(y=y,x=as.matrix(mtrain),
-                 family="binomial",type.measure="auc", nfolds = 5,parallel=TRUE,lambda=lambda)
-pred <- predict(clf,newx=as.matrix(mtrain), s="lambda.min",type="response")
+cat(rmse(y,mtrain[,i]))
+#full version  
+clf <- cv.glmnet(y=y,x=as.matrix(train),
+                 family="binomial",type.measure="auc", nfolds = 4,parallel=TRUE,lambda=lambda)
+qplot(clf$lambda,clf$cvm)
+mtest[,i] <- predict(clf,newx=as.matrix(test), s="lambda.min",type="response")
+ 
+# glm stacking
+#lambda <-10^seq(-5,-1.5,0.01) 
+#clf <- cv.glmnet(y=y,x=as.matrix(mtrain[,1:6]),
+#                 family="binomial",type.measure="auc", nfolds = 4,parallel=TRUE,lambda=lambda)
+#qplot(clf$lambda,clf$cvm)
+# stacking not work so well for this dataset, not as good as simple averaging~
 
-rmse(y,pred)     
-rmse(y,(2*mtrain[,1]+1*mtrain[,2]+3*mtrain[,3])/6)
+#apply(mtrain[,1:6], 2, function(x) auc(y,x)) #auc
+apply(mtrain[,1:6], 2, function(x) mse(y,x)) #auc
+pred_train <- (4*mtrain[,1]+1*mtrain[,2]+1*mtrain[,3]+8*mtrain[,4]+2*mtrain[,5])/16;mse(y,pred_train)
+#sum(1-abs(y-pred_train)/2)/length(pred) 82.2% prediction correct
 
-apply(mtrain, 2, function(x) rmse(y,x))
-# level 1 stacking
-set.seed(1229)
-#idx <- sample(1:nrow(train), nrow(train), replace =FALSE)
-#train <- train[idx,]; y <- y[idx]
-dtrain <- xgb.DMatrix(as.matrix(cbind(mtrain)), label = y)
-param <- list(booster ="gbtree", objective = 'binary:logistic', eval_metric = 'rmse',
-              nthread = 6,eta = 0.01, colsample_bytree = 0.4, subsample = 0.8, max_depth = 6, min_child_weight=11)
-bst.cv = xgb.cv(param=param, data = dtrain, nfold = 5, nrounds = 5000, early.stop.round = 20)
-tmp <- bst.cv$test.rmse.mean; n=which(tmp==min(tmp))[1]
+#pred <- as.integer(pred_train>0.756)
+#head(as.integer(pred_train>0.756),10)
 
-#[716]	train-rmse:0.407825+0.000765	test-rmse:0.419664+0.003077
-#[437]	train-rmse:0.402716+0.000767	test-rmse:0.419333+0.003904
-#no dummy [917]	train-rmse:0.403022+0.000767	test-rmse:0.419341+0.003321
-#tmp <- bst.cv$test.rmse.mean; n=which(tmp==min(tmp))[1];n
-#bst <- xgb.train(params=param, data=dtrain, nround = as.integer(n/0.8))
-#pred <- predict(bst, as.matrix(testx)) 
+#submission 
+pred_test <- (4*mtest[,1]+1*mtest[,2]+1*mtest[,3]+8*mtest[,4]+2*mtest[,5])/16
+submission <- data.frame(ID=testID, readmitted= as.numeric(format(pred_test,digits = 16,scientific = FALSE)))
+head(submission)
+write.csv(submission, "validation_readmitted.csv", row.names=FALSE)
+  
--- single_molel.R
+++ single_molel.R
@@ -1,107 +1,56 @@
-cat("hospital readmission data explore")
-cat("By Youliang Yu")
-cat("Nov 11, 2016")
+#############################################################################
+#title: Predicting Hospital Readmissions, Hack K-State, 2016               ##
+#author: Youliang Yu                                                       ##
+#date: Nov. 12, 2016                                                       ##      
+#############################################################################
 
 library(caret)  
 library(data.table) 
-#library(bit64)
 library(xgboost)
 library(Metrics)
-library(gtools)
-
-setwd("/home/youliang/computing/hackingkstate/challenge1") 
-#cat("first look at the train data...")
-#train <- fread('data/Challenge_1_Training.csv')
-#cat("missing value specified as (No,NO,None,?)")
-#cat("seems a lot missing value, need to figure out how to deal with them, impute 0 as in the first model")
 
 train <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,
                na.strings=c("?","No","NO","None",""),data.table= FALSE)
-y <- train[!is.na(train$readmitted),50]
-y <- as.integer(as.factor(y))-1
-trainx <- train[!is.na(train$readmitted),-c(1,2)]
-trainx[["readmitted"]] <- as.integer(as.factor(trainx$readmitted))-1
+test <- fread('test.csv',header =TRUE,stringsAsFactors = FALSE,
+               na.strings=c("?","No","NO","None",""),data.table= FALSE)
+train <- train[!is.na(train$readmitted),] #rm label NA
+y <- as.integer(as.factor(train$readmitted))-1
+train <- train[,!names(train) %in% c("encounter_id","patient_nbr","readmitted")] #remove IDs 
+testID <- test$patient_nbr
+test <- test[,!names(test) %in% c("encounter_id","patient_nbr")] 
 
-char_vars <- colnames(trainx)[sapply(trainx, is.character)]
-
-# replace var with target mean
-for(var in char_vars) {
-  #  if(is.character(x.test[[var]])) {
-  #    target.mean <- trainx[, list(pr=mean(target)), by=eval(var)]
-  #    x.test[[var]] <- target.mean$pr[match(x.test[[var]], target.mean[[var]])] 
-  temp <- rep(NA, nrow(trainx))
-  print(var) 
-  for(i in 1:4) {   
-    ids.1 <- -seq(i, nrow(trainx), by=4)
-    ids.2 <- seq(i, nrow(trainx), by=4)
-    levels <- unique(trainx[ids.2,var])
-    target.means <- unlist(lapply(levels, function(x) x=mean(trainx[ids.1,"readmitted"][trainx[ids.1,var]==x],na.rm = T)))
-    names(target.means) <- levels
-    temp[ids.2][!is.na(trainx[ids.2,var])] <- unlist(lapply(trainx[ids.2,var][!is.na(trainx[ids.2,var])], function(x) x = target.means[[x]]))
-  }
-  trainx[[var]] <- temp
-  #  }
-}
-
-# factorize
-for (f in names(trainx)) {
-  if (class(trainx[[f]])=="character") {
-    levels <- unique(c(trainx[[f]]))
-    trainx[[f]] <- as.integer(factor(trainx[[f]], levels=levels)) 
+# factorize char vars
+fact_vars <- colnames(train)[sapply(train, is.character)]
+for (f in fact_vars) {
+  if (class(train[[f]])=="character") {
+    levels <- unique(c(train[[f]],test[[f]]))
+    train[[f]] <- as.integer(factor(train[[f]], levels=levels)) 
+    test[[f]] <- as.integer(factor(test[[f]], levels=levels)) 
   }
 }
 
-# get features gives too much NA(>9999%)
-#apply(trainx, 2, function(x)  sum(is.na(x))/nrow(trainx))
-na_var <- names(trainx)[apply(trainx, 2, function(x)  sum(is.na(x))/nrow(trainx)>0.99999)]
-for (f in na_var){trainx[[f]] <- NULL}
+#remove constant features
+cst_vars <- names(train)[apply(train, 2, function(x) length(unique(x))==1)]
+for (f in cst_vars){train[[f]] <- NULL; test[[f]] <- NULL}
+
+# remove features with all NA(>99.99%)
+na_var <- names(train)[apply(train, 2, function(x)  sum(is.na(x))/nrow(train)>0.9999)]
+for (f in na_var){train[[f]] <- NULL; test[[f]] <- NULL}
 
 # impute NA
-trainx[is.na(trainx)] <- 0
-# apply(trainx,2,class)
+train[is.na(train)] <- 0; test[is.na(test)] <- 0
 
-# set 10% holdout
-set.seed(1229)
-idx <- sample(1:nrow(trainx), 0.1*nrow(trainx), replace =FALSE)
-testx <- trainx[idx,]
-trainx <- trainx[-idx,]
-y <- as.integer(as.factor(y))-1
-testy <- y[idx];trainy <- y[-idx]
-# set 10% holdout ends
-
-# adding 2-way interaction
-cmb <- combinations(n=length(char_vars), r=2, v=char_vars)
-for(i in 1:nrow(cmb)) {
-  trainx[[paste0(cmb[i,1], cmb[i,2])]] <- paste(trainx[[cmb[i,1]]], trainx[[cmb[i,2]]]) 
-}
-for (f in names(trainx)) {
-  if (class(trainx[[f]])=="character") {
-    levels <- unique(c(trainx[[f]]))
-    trainx[[f]] <- as.integer(factor(trainx[[f]], levels=levels)) 
-  }
-} 
-# adding 2-way interaction ends
-
-#xgb model
-set.seed(1229)
-
-dtrain <- xgb.DMatrix(as.matrix(cbind(rbind(trainx,testx)[,-ncol(trainx)],)), label = rbind(trainx,testx)[,ncol(trainx)])
+#build xgb model
+set.seed(1229);dtrain <- xgb.DMatrix(as.matrix(train), label = y)
 param <- list(booster ="gbtree", objective = 'binary:logistic', eval_metric = 'rmse',
-              nthread = 6,eta = 0.05, colsample_bytree = 1, subsample = 0.8, max_depth = 6, min_child_weight=11)
+              nthread = 6,eta = 0.01, colsample_bytree = 0.4, subsample = 0.8, max_depth = 6, min_child_weight=11)
+#bst.cv = xgb.cv(param=param, data = dtrain, nfold = 5, nrounds = 5000, early.stop.round = 50)
+#[460]	train-rmse:0.402558+0.000524	test-rmse:0.419567+0.003207
+#tmp <- bst.cv$test.rmse.mean; n=which(tmp==min(tmp))[1]
+n=913;bst <- xgb.train(params=param, data=dtrain, nround = as.integer(n/0.8))
+pred <- predict(bst, as.matrix(test));print(pred)
 
-bst.cv = xgb.cv(param=param, data = dtrain, nfold = 10, nrounds = 1000, early.stop.round = 15)
-# basic model CV colsample=1, [120]	train-rmse:0.396306+0.001360	test-rmse:0.421042+0.004926
-#                colsample=1,subsample=0.8,[93]	train-rmse:0.397728+0.000859	test-rmse:0.420347+0.003593
-#                colsample=1,subsample=0.8,max_depth=6,[104]	train-rmse:0.395256+0.000949	test-rmse:0.420451+0.003026
-#                colsample=1,subsample=0.8,max_depth=6,min_child_weight=11,[106]	train-rmse:0.402792+0.000956	test-rmse:0.420202+0.002904
-#                                                                 10 folds,[117]	train-rmse:0.402395+0.000709	test-rmse:0.420096+0.006663
-
-# target-mean comparable
-
-bst <- xgb.train(params=param, data=dtrain, nround = 120)
-pred <- predict(bst, as.matrix(testx))
-#plot(pred)
-rmse(testy,pred)
-#print(pred <- predict(bst, as.matrix(testx[1,])))
-
- 
+#output csv
+submission <- data.frame(ID=testID, readmitted= as.numeric(format(pred,digits = 16,scientific = FALSE)))
+head(submission)
+write.csv(submission, "test_readmitted.csv", row.names=FALSE)
